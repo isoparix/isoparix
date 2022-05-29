e5701d31b304f700fee9bd14935dfd5b8a280617
John Watts <john_watts@isoparix.com>
2022-05-29 12:38:49 +0100

    Have added an XMapWindow call when the Configure notification happens. Adds a bit of overhead, but not much.

--- picker.f
+++ picker.f
@@ -72,9 +72,8 @@ c
       if(resize
      *  )then
              call tim(t_now)
-             if(nbut.eq.-994
-c            if(nbut.eq.-10
-c            if(t_now-t_cn.gt.1.0
+             if(nbut.eq.-994.or.
+     *          nbut.eq.-10
      *         )then
                     iwidth=iwidthr
                     iheight=iheightr
